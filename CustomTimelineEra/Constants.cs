96eccdb6b9c1d84eb3ffd99b54fde9c577445de8
Corey Smith <coreyas@gmail.com>
2017-07-15 16:45:13 -0400

    Identify all visits as "bruce.wayne@wayneenterprises.com"

---
+++ CustomTimelineEra/Constants.cs
@@ -0,0 +1,9 @@
+﻿using Sitecore.Configuration;
+
+namespace CustomTimelineEra
+{
+  public static class Constants
+  {
+    public static readonly string ContactEmail = Settings.GetSetting("CustomTimelineEra.ContactEmail");
+  }
+}
---
+++ CustomTimelineEra/Pipelines/CreateVisit/IdentifyContact.cs
@@ -0,0 +1,16 @@
+﻿using Sitecore.Analytics.Model;
+using Sitecore.Analytics.Pipelines.CreateVisits;
+
+namespace CustomTimelineEra.Pipelines.CreateVisit
+{
+  public class IdentifyContact : CreateVisitProcessor
+  {
+    public override void Process(CreateVisitArgs args)
+    {
+      var session = args.Session;
+      if (session.Contact.Identifiers.IdentificationLevel == ContactIdentificationLevel.Known) return;
+
+      session.Identify(Constants.ContactEmail);
+    }
+  }
+}
