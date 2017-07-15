a167eec086283895a6ae64d9a859d84558f9e3e9
Corey Smith <coreyas@gmail.com>
2017-07-15 17:26:56 -0400

    Populate built-in facets with contact information

---
+++ CustomTimelineEra/Facets.cs
@@ -0,0 +1,45 @@
+﻿namespace CustomTimelineEra
+{
+  public static class Facets
+  {
+    public static class ContactAddresses
+    {
+      public const string Addresses = "Addresses";
+
+      public static class Keys
+      {
+        public const string Home = "Home address";
+      }
+    }
+
+    public static class ContactEmailAddresses
+    {
+      public const string Emails = "Emails";
+
+      public static class Keys
+      {
+        public const string Home = "Home email";
+      }
+    }
+
+    public static class ContactPersonalInfo
+    {
+      public const string Personal = "Personal";
+    }
+
+    public static class ContactPhoneNumbers
+    {
+      public const string PhoneNumbers = "Phone Numbers";
+
+      public static class Keys
+      {
+        public const string Home = "Home phone";
+      }
+    }
+
+    public static class ContactPicture
+    {
+      public const string Picture = "Picture";
+    }
+  }
+}
