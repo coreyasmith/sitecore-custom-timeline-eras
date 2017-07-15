d203a4022f27391cbf9234c7cc39f3c2a7141ec9
Corey Smith <coreyas@gmail.com>
2017-07-15 17:32:50 -0400

    Add button to abandon session so data appears in Experience Profile immediately

---
+++ CustomTimelineEra/Controllers/SessionController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using CustomTimelineEra.Infrastructure;
+
+namespace CustomTimelineEra.Controllers
+{
+  public class SessionController : BaseController
+  {
+    [HttpPost]
+    public ActionResult AbandonSession()
+    {
+      Session.Abandon();
+      return RedirectToReferrer();
+    }
+  }
+}
---
+++ CustomTimelineEra/Infrastructure/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace CustomTimelineEra.Infrastructure
+{
+  public abstract class BaseController : Controller
+  {
+    public RedirectResult RedirectToReferrer()
+    {
+      var redirectUrl = Request.UrlReferrer;
+      return redirectUrl == null ? new RedirectResult(Request.Url?.ToString()) : new RedirectResult(redirectUrl.ToString());
+    }
+  }
+}
