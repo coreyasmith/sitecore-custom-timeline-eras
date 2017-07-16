65736f93c2a8c84d7681781810a173e1e75f544e
Corey Smith <coreyas@gmail.com>
2017-07-16 16:35:49 -0400

    Rename "Unknown Contact" era in Experience Profile timeline to "Born"

---
+++ CustomTimelineEra/Pipelines/Journey/RenameAnonymousEra.cs
@@ -0,0 +1,29 @@
+﻿using System.Data;
+using System.Linq;
+using Sitecore.Cintel.Reporting;
+using Sitecore.Cintel.Reporting.Contact.Journey;
+using Sitecore.Cintel.Reporting.Processors;
+
+namespace CustomTimelineEra.Pipelines.Journey
+{
+  public class RenameAnonymousEra : ReportProcessorBase
+  {
+    public string AnonymousEraName { get; set; }
+
+    public override void Process(ReportProcessorArgs args)
+    {
+      var resultTableForView = args.ResultTableForView;
+      RenameAnonymousEraInTimeline(resultTableForView);
+    }
+
+    private void RenameAnonymousEraInTimeline(DataTable resultTable)
+    {
+      var dataRows = resultTable.AsEnumerable();
+      var anonymousEras = dataRows.Where(r => r.Field<string>(Schema.EraText.Name) == "Unknown Contact");
+      foreach (var anonymousEra in anonymousEras.ToList())
+      {
+        anonymousEra.SetField(Schema.EraText.Name, AnonymousEraName);
+      }
+    }
+  }
+}
