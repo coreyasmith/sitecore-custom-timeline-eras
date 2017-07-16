dcd6df15a8c0f26750878a45c38043ac9b35bd20
Corey Smith <coreyas@gmail.com>
2017-07-16 18:02:39 -0400

    Add functionality to show custom outcomes as eras in Experience Profile timeline

---
+++ CustomTimelineEra/Templates.cs
@@ -0,0 +1,15 @@
+﻿using Sitecore.Data;
+
+namespace CustomTimelineEra
+{
+  public static class Templates
+  {
+    public static class CustomOutcomeDefinition
+    {
+      public static class Fields
+      {
+        public static readonly ID ShowAsEra = new ID("{C9F25AA6-2254-4792-A91F-289C86F52B22}");
+      }
+    }
+  }
+}
