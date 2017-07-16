7e50d3baef5e60de2d9ac344875cba7eb8188f68
Corey Smith <coreyas@gmail.com>
2017-07-16 16:32:50 -0400

    Remove "Unknown Contact" era from Experience Profile timeline

---
+++ CustomTimelineEra/Pipelines/Journey/RemoveAnonymousEra.cs
@@ -0,0 +1,36 @@
+﻿using System.Data;
+using System.Linq;
+using Sitecore.Cintel.Reporting;
+using Sitecore.Cintel.Reporting.Contact.Journey;
+using Sitecore.Cintel.Reporting.Processors;
+
+namespace CustomTimelineEra.Pipelines.Journey
+{
+  public class RemoveAnonymousEra : ReportProcessorBase
+  {
+    public bool ShowIcon { get; set; }
+
+    public override void Process(ReportProcessorArgs args)
+    {
+      var resultTableForView = args.ResultTableForView;
+      RemoveAnonymousEraFromTimeline(resultTableForView);
+    }
+
+    private void RemoveAnonymousEraFromTimeline(DataTable resultTable)
+    {
+      var dataRows = resultTable.AsEnumerable();
+      var anonymousEras = dataRows.Where(r => r.Field<string>(Schema.EraText.Name) == "Unknown Contact");
+      foreach (var anonymousEra in anonymousEras.ToList())
+      {
+        if (ShowIcon)
+        {
+          anonymousEra.SetField(Schema.EventType.Name, "Outcome");
+        }
+        else
+        {
+          resultTable.Rows.Remove(anonymousEra);
+        }
+      }
+    }
+  }
+}
